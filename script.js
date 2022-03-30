249225e288d5c2038f980dbe38e15b76f4bec0d3
Iury Assunção <iuryifba@gmail.com>
2022-03-30 20:38:47 -0300

    funcionalidade remover finalizada

--- script.js
+++ script.js
@@ -7,12 +7,24 @@ let inputNovaTarefa = document.getElementById('nova_tarefa');
 addBotao.addEventListener('click',(e)=>{
     e.preventDefault();
 
+    let valida = inputNovaTarefa.value;
+
+    if(valida != ''){
         let tarefas = {
             nome: inputNovaTarefa.value,
             id:gerarId(),
         }
 
         adicionarTarefa(tarefas);
+        
+
+    }
+
+    else{
+        window.alert("Insira uma nova tarefa");
+    }
+
+        
 });
 
 function gerarId(){
@@ -25,10 +37,12 @@ function adicionarTarefa(tarefas){
     
     inputNovaTarefa.value = '';
 
+    
 }
 
 function criarLi(tarefas){
     let li = document.createElement('li');
+    li.id = tarefas.id;
     
 
     let p = document.createElement('p');
@@ -39,12 +53,29 @@ function criarLi(tarefas){
     let btnRemover = document.createElement('button');
     btnRemover.innerHTML = `<img src="./imgs/remove.png"/>`;
 
+    btnRemover.setAttribute('onclick','removerTarefa('+tarefas.id+')');
+
     li.appendChild(p);
     li.appendChild(btnRemover);
 
     return li;
 
+}
 
+function removerTarefa(id){
+    let confirma = window.confirm('Tem certeza que deseja excluir?');
+
+    if(confirma){
+
+        let li = document.getElementById(''+id+ '');
+        console.log(id)
+        if(li) {
+            tarefa.removeChild(li);
+        } else {
+            alert(id);
+        }
+
+    };
 }
 
 
